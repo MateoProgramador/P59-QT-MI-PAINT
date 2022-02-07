59ee0a1d3f64dd357e4a7a708e21127a6d92e315
Mateo Programador <mramirezv1@est.ups.edu.ec>
2022-02-06 23:33:49 -0500

    Figura rectangulo y circulo

--- principal.h
+++ principal.h
@@ -14,8 +14,42 @@ class Principal : public QMainWindow
 public:
     Principal(QWidget *parent = nullptr);
     ~Principal();
+    void Rectangulo();
+    int op;
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+
+private slots:
+    void on_actionAncho_triggered();
+
+    void on_actionSalir_triggered();
+
+    void on_actionColor_triggered();
+
+    void on_actionNuevo_triggered();
+
+    void on_actionGuardar_triggered();
+
+    void on_actionRect_nculos_triggered();
+
+    void on_actionCircunferencias_triggered();
+
+    void on_actionLineas_triggered();
 
 private:
     Ui::Principal *ui;
+    QImage *mImagen;        // Imagen sobre la que se va a dibujar
+    QPainter *mPainter;     // Painter de la imagen
+    QPoint mInicial;        // Punto incial para dibujar la línea
+    QPoint mFinal;          // Punto final para dibujar la línea
+    bool mPuedeDibujar;     // Determina si debe o no dibujar
+    int mAncho;             // Define el ancho del pincel
+    QColor mColor;          // Define el color del pincel
+    int mNumLineas;         // Cuenta el número de líneas
 };
 #endif // PRINCIPAL_H
